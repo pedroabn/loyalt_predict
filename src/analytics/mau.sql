68ee51836fa95db9709e096f9ffc4aacb2025df2
Pedro Neiva <pedroabneiva20@gmail.com>
2026-01-20 22:20:43 -0300

    Mau e Dau - SQL

---
+++ src/analytics/mau.sql
@@ -0,0 +1,25 @@
+-- Quantidade de usuários ativos por mês, de novos clientes ativos por dia
+with tb_daily AS (
+    SELECT DISTINCT 
+        date(SUBSTR(DtCriacao, 0, 11)) AS Dtdia,
+        IdCliente
+    FROM transacoes
+),
+
+tb_distinct AS (
+    SELECT DISTINCT Dtdia as dtRef
+    FROM tb_daily
+)
+
+SELECT t1.dtRef,
+        count (DISTINCT IdCliente) as MAU,
+        count (DISTINCT t2.Dtdia) as qtdeDias
+
+FROM tb_distinct as t1
+LEFT JOIN tb_daily as t2
+ON t2.Dtdia <= t1.dtRef
+AND julianday(t1.dtRef) - julianday(t2.Dtdia) < 28
+
+GROUP BY t1.dtRef
+ORDER BY t1.dtRef DESC
+LIMIT 100;
