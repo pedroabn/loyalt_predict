3904013ed094fec4ad7fdbe01f6b9164db4d2481
Pedro Neiva <pedrodeabn@gmail.com>
2026-02-05 10:31:47 -0300

    É isso

--- src/analytics/predict-fiel.py
+++ src/analytics/predict-fiel.py
@@ -3,19 +3,20 @@ import pandas as pd
 import sqlalchemy
 import mlflow
 #%%
-con = sqlalchemy.create_engine('sqlite:///../../data/analytics/database.db')
-mlflow.set_tracking_uri("http://localhost:5000")
+def DBPredict():
+    con = sqlalchemy.create_engine('sqlite:///../../data/analytics/database.db')
+    mlflow.set_tracking_uri("http://localhost:5000")
 
-versions = mlflow.search_model_versions(filter_string="name='model_fiel'")
-last_version = max((int(i.version) for i in versions))
+    versions = mlflow.search_model_versions(filter_string="name='model_fiel'")
+    last_version = max((int(i.version) for i in versions))
 
-model = mlflow.sklearn.load_model(f"models:///model_fiel/{last_version}")
-data = pd.read_sql("SELECT * FROM fs_all", con)
-print(f"Dados carregados: {data.shape}")
+    model = mlflow.sklearn.load_model(f"models:///model_fiel/{last_version}")
+    data = pd.read_sql("SELECT * FROM fs_all WHERE descLifeCycleFoto = '03-TURISTA'", con)
+    print(f"Dados carregados: {data.shape}")
 
-# Tudo em 3 linhas:
-data['predictFiel'] = model.predict_proba(data[model.feature_names_in_])[:, 3]
-data_output = data[['dtRef', 'IdCliente', 'predictFiel']]
-    
+    # Tudo em 3 linhas:
+    data['predictFiel'] = model.predict_proba(data[model.feature_names_in_])[:, 1]
+    data_output = data[['dtRef', 'IdCliente', 'predictFiel']]
+    return data_output
 #%%
 data = DBPredict()
